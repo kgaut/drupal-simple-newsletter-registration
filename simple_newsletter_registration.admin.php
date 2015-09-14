f5bcd6dfc6a3673b8334805ac1ca5ef04718af42
Kevin Gautreau <contact@kgaut.net>
2015-09-14 08:55:54 +0200

    refactoring

--- simple_newsletter_registration.admin.php
+++ simple_newsletter_registration.admin.php
@@ -1,49 +1,27 @@
 <?php
 
-function _silhouette_newsletter_admin_form($form, &$form_state) {
+function _simple_newsletter_registration_admin_form($form, &$form_state) {
   $langues = language_list();
-  $form['silhouette_newsletter_redirection_default'] = array(
+  $form['snr_block_title'] = array(
     '#type' => 'textfield',
-    '#title' => t('Page de redirection par default'),
-    '#default_value' => variable_get('silhouette_newsletter_redirection_default'),
-    '#description' => t('Le nid de la page de remerciement par default pour le formulaire d\'inscription à la newsletter'),
-    '#required' => TRUE,
-    '#element_validate' => array('_silhouette_newsletter_validate_nid'),
-  );
-
-  foreach ($langues as $l) {
-    $form['silhouette_newsletter_redirection_' . $l->language] = array(
-      '#type' => 'textfield',
-      '#title' => t('Page de redirection @lang', array('@lang' => $l->native)),
-      '#default_value' => variable_get('silhouette_newsletter_redirection_' . $l->language),
-      //'#description' => t('la page facebook qui sera utilisé s\'il n\'y en a pas pour la langue active'),
-      '#required' => FALSE,
-      '#element_validate' => array('_silhouette_newsletter_validate_nid'),
-    );
-  }
-
-  $form['silhouette_newsletter_texte_default'] = array(
-    '#type' => 'textarea',
-    '#title' => t('Texte d\'introduction par défaut'),
-    '#default_value' => variable_get('silhouette_newsletter_texte_default'),
-    '#description' => t('Le texte affiché en haut du formulaire d\'inscription à la newsletter'),
+    '#title' => t('Newsletter registration block title'),
+    '#default_value' => variable_get('snr_block_title'),
     '#required' => TRUE,
   );
+  $form['snr_landing_page'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Landing page after registration'),
+    '#default_value' => variable_get('snr_landing_page'),
+    '#description' => t('ie : node/28'),
+    '#required' => TRUE,
+    '#element_validate' => array('_simple_newsletter_registration_validate_nid'),
+  );
 
-  foreach ($langues as $l) {
-    $form['silhouette_newsletter_texte_' . $l->language] = array(
-      '#type' => 'textarea',
-      '#title' => t('Texte d\'introduction @lang', array('@lang' => $l->native)),
-      '#default_value' => variable_get('silhouette_newsletter_texte_' . $l->language),
-      '#required' => FALSE,
-    );
-
-  }
   return system_settings_form($form);
 
 }
 
-function _silhouette_newsletter_validate_nid($element, &$form_state) {
+function _simple_newsletter_registration_validate_nid($element, &$form_state) {
   $value = $element['#value'];
   if (isset($value) && $value != '') {
     if (is_numeric($value)) {
@@ -61,7 +39,7 @@ function _silhouette_newsletter_validate_nid($element, &$form_state) {
   }
 }
 
-function _silhouette_newsletter_listing_form($form, &$form_state) {
+function _simple_newsletter_registration_listing_form($form, &$form_state) {
   if (isset($form_state['input']['submit_export']) && trim($form_state['input']['submit_export']) != '') {
     $mode = "EXPORT";
   }
@@ -69,15 +47,15 @@ function _silhouette_newsletter_listing_form($form, &$form_state) {
     $mode = "DISPLAY";
   }
 
-  $query = db_select('sil_newsletter_registration', 'nr');
+  $query = db_select('newsletter_registration', 'nr');
   $query = $query->extend('TableSort');
   $query->fields('nr',array('snr'));
   $query->orderBy('nr.snr', 'DESC');
   $result = $query->execute();
   $options = array();
-  $headers = _silhouette_newsletter_listing_form_header();
+  $headers = _simple_newsletter_registration_listing_form_header();
   foreach ($result as $i) {
-    $options[$i->snr] = _silhouette_newsletter_listing_form_option($i->snr);
+    $options[$i->snr] = _simple_newsletter_registration_listing_form_option($i->snr);
   }
 
   if($mode == 'EXPORT') {
@@ -118,42 +96,27 @@ function _silhouette_newsletter_listing_form($form, &$form_state) {
   }
 }
 
-function _silhouette_newsletter_listing_form_header() {
+function _simple_newsletter_registration_listing_form_header() {
   $header = array(
     'snr' => array('data' => t('ID')),
-    'language' => array('data' => t('Langue')),
-    'civility' => array('data' => t('Civilité')),
-    'firstname' => array('data' => t('Prénom')),
-    'lastname' => array('data' => t('Nom de famille')),
-    'email' => array('data' => t('Adresse e-mail')),
-    'adherent' => array('data' => t('Adhérent ?')),
-    'registration_number' => array('data' => t('Numéro d\'adhérent')),
-    'club' => array('data' => t('Club')),
+    'email' => array('data' => t('E-mail address')),
     'status' => array('data' => t('Status')),
-    'created' => array('data' => t('Inscrit le')),
-    'updated' => array('data' => t('Maj le')),
-    'actions' => array('data' => t('Actions')),
+    'created' => array('data' => t('Created')),
+    'updated' => array('data' => t('Updated')),
+    //'actions' => array('data' => t('Actions')),
   );
   return $header;
 }
 
-function _silhouette_newsletter_listing_form_option($snr) {
+function _simple_newsletter_registration_listing_form_option($snr) {
   $inscrit = new InscritNewsletter(array('snr' => $snr));
-  $club = node_load($inscrit->club);
   return array(
     'snr' => $inscrit->snr,
-    'language' => $inscrit->language,
-    'civility' => $inscrit->civility,
-    'firstname' => $inscrit->firstname,
-    'lastname' => $inscrit->lastname,
     'email' => $inscrit->email,
-    'adherent' => $inscrit->adherent == 1 ? 'oui' : "non",
-    'registration_number' => $inscrit->registration_number,
-    'club' => isset($club->title) ? $club->title : '',
     'status' => $inscrit->status == 1 ? t('Inscrit') : t('Désinscrit'),
     'created' => $inscrit->created ? format_date($inscrit->created, 'short') : NULL,
     'updated' => $inscrit->updated ? format_date($inscrit->updated, 'short') : NULL,
-    'actions' => l('Modifier','admin/silhouette_newsletter/'.$inscrit->snr.'/edit'). ' ' . l('Supprimer','admin/silhouette_newsletter/'.$inscrit->snr.'/delete',array('attributes'=>array('onclick'=>'return confirm("Êtes-vous sûr?")'))),
+    //'actions' => l('Edit','admin/simple_newsletter_registration/'.$inscrit->snr.'/edit'). ' ' . l('Delete','admin/simple_newsletter_registration/'.$inscrit->snr.'/delete',array('attributes'=>array('onclick'=>'return confirm("Êtes-vous sûr?")'))),
   );
 }
 
@@ -161,7 +124,7 @@ function locations_listing_content_form_submit($form, &$form_state) {
   $form_state['rebuild'] = TRUE;
 }
 
-function _silhouette_newsletter_edit($form, &$form_state) {
+function _simple_newsletter_registration_edit($form, &$form_state) {
   $snr = $form_state['build_info']['args'][0];
   if(!is_numeric($snr) || $snr <= 0) {
     drupal_access_denied();
@@ -173,12 +136,12 @@ function _silhouette_newsletter_edit($form, &$form_state) {
     exit;
   }
   else {
-    $form = drupal_get_form('_silhouette_newsletter_form',$inscrit);
+    $form = drupal_get_form('_simple_newsletter_registration_form',$inscrit);
     return $form;
   }
 
 }
-function _silhouette_newsletter_delete($snr) {
+function _simple_newsletter_registration_delete($snr) {
   if(!is_numeric($snr) || $snr <= 0) {
     drupal_access_denied();
     exit;
@@ -188,7 +151,7 @@ function _silhouette_newsletter_delete($snr) {
   $inscrit->delete();
 
   drupal_set_message('L\'enregistrement à été supprimé');
-  drupal_goto('admin/silhouette_newsletter');
+  drupal_goto('admin/simple_newsletter_registration');
 
 
 }
